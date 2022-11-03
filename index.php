47e3e9aeb37ad20d3bfaf8e8a86cefea1054f0c0
Toshif <toshifpatil401@gmail.com>
2022-11-03 20:32:33 +0530

    commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("./navbar.php")  ?>
+<style>
+     img{
+        position: fixed;
+        background-color: green;
+        width: 100%;
+    }
+</style>
+<img src="./assets/map.jpg" width="100%" alt="">
